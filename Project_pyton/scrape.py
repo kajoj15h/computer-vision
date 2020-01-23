c85f001628553d69a62a1575ea0d47fdfd67606d
kkrol <kajoj15hh@gmail.com>
2020-01-23 17:58:31 +0100

    pyton

---
+++ Project_pyton/scrape.py
@@ -0,0 +1,39 @@
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+search_querries = ['black dress', 'white dress', 'black shoes', 'white shoes', 'black sweatshirt','red sweatshirt', 'red t-shirt', 'red dress']
+
+
+def download_images(query):
+    arguments = {
+        'keywords': query,
+        'format': 'jpg',
+        'limit': 100,
+        'print_urls': True,
+        'size': 'medium',
+        'aspect_ratio': 'square',
+        'chromedriver': r'‪C:\Users\kkrol\Desktop\chromedriver\chromedriver.exe'
+    }
+
+    try:
+        response.download(arguments)
+    except FileNotFoundError:
+        arguments = {
+            'keywords': query,
+            'format': 'png',
+            'limit': 100,
+            'print_urls': True,
+            'size': 'medium',
+            'chromedriver': r'‪C:\Users\kkrol\Desktop\chromedriver\chromedriver.exe'
+        }
+
+        try:
+            response.download(arguments)
+        except:
+            pass
+
+
+for query in search_querries:
+    download_images(query)
+    print()
